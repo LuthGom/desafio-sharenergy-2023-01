01c9cb509177de5dd69abac5c4ff4b40e643e9be
LuthGom <lucianomendes23@yahoo.com>
2023-01-06 22:02:37 -0300

    Adicionando props buttonName e onClick

--- sharenergy_challenge_backend_jr/src/components/input/Input.tsx
+++ sharenergy_challenge_backend_jr/src/components/input/Input.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { InputContainer } from "../../styled-components/InputStyled";
+import Button from "../button/Button";
 interface Props {
   id: string;
   label: string;
@@ -7,18 +8,22 @@ interface Props {
   name: string;
   placeholder?: string;
   svg?: JSX.Element | JSX.Element[];
+  buttonName?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-
-function Input({ id, label, type, placeholder, svg }: Props) {
+function Input({ id, label, type, placeholder, svg, buttonName, onClick }: Props) {
   return (
     <InputContainer>
       <span className="subContainer">
         <label htmlFor={id}>{label}</label>
-        <span className="placeholder">
-          {svg}
-          <input type={type} id={id} name={id} placeholder={placeholder} />
-        </span>
+       
+          <span className="placeholder">
+            {svg}
+            <input type={type} id={id} name={id} placeholder={placeholder} />
+          {buttonName ? <Button onClick={onClick} nome={buttonName} /> : null}
+          </span>
+    
       </span>
     </InputContainer>
   );
