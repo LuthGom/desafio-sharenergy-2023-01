5ceee76399e48f8c05de37e546a20ce73d30042f
LuthGom <lucianomendes23@yahoo.com>
2023-01-06 22:03:35 -0300

    Alterando font-size da label a add customização para el button

--- sharenergy_challenge_backend_jr/src/styled-components/InputStyled.tsx
+++ sharenergy_challenge_backend_jr/src/styled-components/InputStyled.tsx
@@ -13,30 +13,44 @@ export const InputContainer = styled.span`
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))
       drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   }
+
   span.placeholder {
     display: flex;
     align-items: center;
     border: 1px solid white;
-  }
-  input {
-    width: 300px;
-    height: 45px;
-    background: transparent;
-    border: none;
-  }
-  input::placeholder {
-    font-size: 18px;
-    font-weight: 300;
-    line-height: 20px;
-    color: #fff;
-    padding: 1em;
-  }
 
+    width: 350px;
+  }
   label {
     font-size: 22px;
 
     margin-bottom: 10px;
 
     color: #fff;
+  }
+  input {
+    width: 300px;
+    height: 45px;
+    background: transparent;
+    border: none;
+
+    font-size: 18px;
+
+    color: #fff;
+  }
+  button {
+    background: transparent;
+    border: none;
+    padding: 5px;
+
+    color: white;
+  }
+
+  input::placeholder {
+    font-size: 18px;
+    font-weight: 300;
+    line-height: 20px;
+    color: #fff;
+    padding: 1em;
   }
 `;
