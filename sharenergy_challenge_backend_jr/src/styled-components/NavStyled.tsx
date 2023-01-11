76a30f5672dccb2d2cb92521e28375f3a8ecff10
LuthGom <lucianomendes23@yahoo.com>
2023-01-11 08:36:13 -0300

    Criando a Nav como uma lista em uam sidebar

--- sharenergy_challenge_backend_jr/src/styled-components/NavStyled.tsx
+++ sharenergy_challenge_backend_jr/src/styled-components/NavStyled.tsx
@@ -1,9 +1,23 @@
 import styled from "styled-components";
 
 export const NavStyled = styled.nav`
-  display: flex;
-  justify-content: space-around;
-  gap: 20px;
+  height: 100vh;
+  background-color: white;
+  width: 250px;
+  display: none;
+  ol {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
 
-  margin: 5px;
+    margin: 5px;
+  }
+  li {
+    padding: 0.5rem 1rem;
+    list-style-position: inside;
+    width: 100%;
+  }
+
+ 
 `;
