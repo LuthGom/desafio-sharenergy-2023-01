44cd01efc308eb510e4c3ad79b96a1ea83879c11
LuthGom <lucianomendes23@yahoo.com>
2023-01-11 08:37:30 -0300

    Passando o Icone de Menu para Header

--- sharenergy_challenge_backend_jr/src/styled-components/HeaderStyled.tsx
+++ sharenergy_challenge_backend_jr/src/styled-components/HeaderStyled.tsx
@@ -1,7 +1,27 @@
 import styled from "styled-components";
 
 export const HeaderStyled = styled.header`
-    height: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  svg {
+    display: flex;
+    position: absolute;
+  }
 
+  svg:hover {
+    cursor: pointer;
+    box-shadow: 0px 5px 5px black;
+  }
+
+  button {
+    background: transparent;
+    border: none;
     padding: 5px;
+    width: fit-content;
+    height: fit-content;
+    color: white;
+  }
+  button:hover {
+  }
 `;
