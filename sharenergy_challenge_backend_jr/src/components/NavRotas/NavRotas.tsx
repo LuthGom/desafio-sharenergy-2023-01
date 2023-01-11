f4f887f1b56f1de6f45827317997f942aa105e96
LuthGom <lucianomendes23@yahoo.com>
2023-01-11 08:40:06 -0300

    Transformand As rotas da Nav em uma lista ordenada

---
+++ sharenergy_challenge_backend_jr/src/components/NavRotas/NavRotas.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import NavButton from "../NavButton/NavButton";
+type Props = {};
+
+export default function NavRotas({}: Props) {
+  return (
+    <div>
+      <ol>
+        <li>
+          <NavButton nome="Home" href="/" />
+        </li>
+        <li>
+          <NavButton nome="Random List Users" href="/randonusers" />
+        </li>
+        <li>
+          <NavButton nome="CaHTTP" href="/cahttp" />
+        </li>
+        <li>
+          <NavButton nome="Random Dogs" href="/radomdogs" />
+        </li>
+        <li>
+          <NavButton nome="Cadastro de Usuários" href="/cadastrodeusuarios" />
+        </li>
+      </ol>
+    </div>
+  );
+}
