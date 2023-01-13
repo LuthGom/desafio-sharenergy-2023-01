64508a9c0f080bc2209939427be31932b8c8eac0
LuthGom <lucianomendes23@yahoo.com>
2023-01-12 23:49:42 -0300

    Estruturando Página CatHttp

--- sharenergy_challenge_backend_jr/src/pages/CaHTTP/CaHTTP.tsx
+++ sharenergy_challenge_backend_jr/src/pages/CaHTTP/CaHTTP.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import { cats_http_uri } from "../../services/apis_urls";
+import InputSeach from "../../components/InputSearch/InputSeach";
+import { Status } from "../../types/types";
+import { CatHttpStyled } from "../../styled-components/CatHttpPageStyled";
+const defaultStatus: Status = {
+  status: "100",
+};
+export default function CaHTTP({}) {
+  const [value, setValue] = useState(defaultStatus);
+  const [cat, setCat] = useState("");
+  const reqCatHttp = () => {
+    setCat(cats_http_uri + value.status);
+  };
 
-type Props = {}
+  const onStatusChange = (prop: keyof Status, statusValue: string) => {
+    setValue({ ...value, [prop]: statusValue });
+  };
 
-export default function CaHTTP({}: Props) {
   return (
-    <div>CaHTTP</div>
-  )
+    <CatHttpStyled>
+      <span>
+        <InputSeach
+          value={value.status}
+          placeholder="Digite aqui o status HTTP desejado!"
+          onChange={() => {
+            onStatusChange("status", value.status);
+          }}
+        />
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            reqCatHttp();
+          }}
+        >
+          Search
+        </button>
+      </span>
+      <img src={cat} alt="" />
+    </CatHttpStyled>
+  );
 }
