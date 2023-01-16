2d1d7a494b42ad6d8147dc6e260e19c34c56346f
LuthGom <lucianomendes23@yahoo.com>
2023-01-16 20:01:09 -0300

    Criando container RandomUserListStyled

---
+++ sharenergy_challenge_backend_jr/src/styled-components/RandomUsersListStyled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const RandomUsersListContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  p.page{
+    font-size: 22px;
+    font-weight: bold;
+    color: brown;
+  }
+`;
