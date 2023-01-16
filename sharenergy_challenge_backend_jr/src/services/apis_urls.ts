6456a0718c9be5209c00a16275afe4709e694971
LuthGom <lucianomendes23@yahoo.com>
2023-01-16 19:58:51 -0300

    add random user url

--- sharenergy_challenge_backend_jr/src/services/apis_urls.ts
+++ sharenergy_challenge_backend_jr/src/services/apis_urls.ts
@@ -2,3 +2,5 @@ const VITE_VARIABLES = import.meta.env;
 
 export const cats_http_uri = VITE_VARIABLES.VITE_REACT_CATS_API_URL;
 export const random_dogs_uri = VITE_VARIABLES.VITE_REACT_RANDOM_DOGS_URL;
+export const random_users_uri = VITE_VARIABLES.VITE_REACT_RANDOM_USERS_URL;
+export const random_users_seed = VITE_VARIABLES.VITE_REACT_RANDOM_USERS_SEED1;
