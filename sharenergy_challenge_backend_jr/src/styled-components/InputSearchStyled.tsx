c1b9efe0199627ae63b2eaa2dc1e90af23ee93d0
LuthGom <lucianomendes23@yahoo.com>
2023-01-16 19:59:50 -0300

    Criando container InputSeacrStyled

---
+++ sharenergy_challenge_backend_jr/src/styled-components/InputSearchStyled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const InputSearchStyled = styled.input`
+  margin: 10px;
+  padding: 10px;
+
+
+  background-color: gainsboro;
+
+  &::placeholder{
+    font-size: 15px;
+    font-weight: bold;
+  }
+`;
